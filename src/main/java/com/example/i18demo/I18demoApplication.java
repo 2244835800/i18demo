2d926a40711277c70b57d6a954f8131a1a4192a8
hepo <2244835800@qq.com>
2019-03-29 09:42:46 +0800

    Initial commit

---
+++ src/main/java/com/example/i18demo/I18demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.i18demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18demoApplication.class, args);
+    }
+
+}
